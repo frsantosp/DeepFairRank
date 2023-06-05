a5015687f3f417f43628f742cd4f271230ac769f
frsantosp <frsantospreciado@hotmail.com>
2023-06-05 11:52:20 -0400

    New commit

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# A Multi-Objective Framework for Fair Top-k Node Ranking in Networks
-This repository provides a reference implementation of the experiment result related to "A Multi-Objective Framework for Fair Top-k Node Ranking in Networks" paper. 
-
-python packages requriments:
-```shell-script
-networkx
-numpy
-cvxpy
-pandas
-sklearn
-tqdm
-fairsearchcore
-aif360
-Graph_Sampling
-cvxopt
-```
+# FPRank
--- codes/alg.py
+++ codes/alg.py
@@ -1,72 +1,119 @@
 import numpy as np
-from codes.metrics import average_perception, accuracy
+from codes.metrics import average_perception, weighted_statistical_disparity
+from codes.utils import  generate_bins_matrix, power_mean
 import cvxpy as cp
 from tqdm import tqdm
+import sklearn.metrics as metric
+from cvxopt import matrix, solvers
+from scipy import stats
 
-from codes.utils import power_mean, binarize, generate_bins_matrix
+###########################################
+# FPRank Algorithm
+###########################################
 
+def FPRank(W, s, y, prt, norm = -2, arep = 10, brep = 20, solver = 'SCS', numBins = 10, bin_width = 0.1, arange = [-2,2], brange = [-1,3]):  
+ 
+    k = int(np.sum(y))
+    n, m = W.shape
 
-
-def FPRank(W,s,h_c,prt_group, norm = -10, rep = 40, solver = 'SCS', ):  
-    acceptance_n = int(np.sum(h_c))
-    s_b = binarize(s, acceptance_n)
-    n = W.shape[0]
-    m = W.shape[1]
-
-    B0,B1 = generate_bins_matrix(s,prt_group)
-    B_W = np.diag(np.arange(0,1.1, 0.1))
+    B0,B1 = generate_bins_matrix(s,prt,numBins)
+    B_W = np.diag(np.arange(0,1.1, bin_width))
    
     x = cp.Variable(n)
     z = cp.Variable(m)
     
     error = cp.sum_squares(s-x)
-    #PB = cp.norm(pos_0.T@x - pos_1.T@x, norm)
     GF = cp.quad_form(B0@x-B1@x, B_W)
     
     alpha = cp.Parameter(nonneg=True)
     beta = cp.Parameter(nonneg=True)
     norm_z = cp.norm(z, 2)
 
-    obj = cp.Minimize( error + alpha*GF + beta*norm_z ) 
-    constraints = [0 <= x, x<= 1, 0<= z, W@x  <=x + z ]
+    obj = cp.Minimize(error + alpha*GF + beta*norm_z)
+    constraints = [0 <= x, x<= 1, 0<= z, W@x <= x + z ]
     prob = cp.Problem(obj, constraints)
 
-    alpha_vals = np.logspace(0, 1, 10)
-    beta_vals = np.logspace(-2, 2, rep)
+    params = {'alpha': list(), 'beta': list()}
+    results = {'corr': list(), 'precision': list(), 'fairperception': list(), 'wsd': list(), 'wrd': list(), 'h': list()}
     
-    tavs = list()
-    results = list()
-    for val in tqdm(alpha_vals):
+    for val in tqdm(np.logspace(arange[0], arange[1], arep)):
         alpha.value = val
-        for val_b in beta_vals:   
+        for val_b in np.logspace(brange[0], brange[1], brep):   
             beta.value = val_b
             try:
+                params['alpha'].append(val)
+                params['beta'].append(val_b)
+
                 prob.solve(solver= solver)
                 h = x.value
-                h_b = binarize(h, acceptance_n)
-                fv = average_perception(W, h_b)
-                acc_hat = accuracy(h_b,s_b)
-                results.append(h)
-                tav = power_mean([acc_hat, fv], norm)
-                tavs.append(tav)
+                
+                fv = average_perception(W, h)
+                corr = stats.spearmanr(h, s).statistic
+                wsd = weighted_statistical_disparity(h, B0, B1, bin_width)
+                wrd = weighted_statistical_disparity(h, B0, B1, bin_width, toRank=True)                
+                prec = metric.average_precision_score(y, h)
+                
+                results['h'].append(h)
+                results['corr'].append(corr)
+                results['precision'].append(prec)
+                results['fairperception'].append(fv)
+                results['wsd'].append(wsd)
+                results['wrd'].append(wrd)
             except:
                 pass     
-    i = np.argmax(tavs)
-    return results[i]
+            print('  alpha={:.4f}, beta={:.4f}: prec={:.4f}, corr={:.4f}, fp={:.4f}, wsd={:.6f}, wrd={:.6f}'.format(val, val_b, prec, corr, fv, wsd, wrd))
+            
+    return results, params
 
+###########################################
+# Hyperparameter tuning for FPRank Algorithm
+###########################################
 
-def MaxPerception(W,y,h_c,prt_group, solver = 'SCS'):
-    acceptance_n = int(np.sum(h_c))
-    n = W.shape[0]
-   
-    x = cp.Variable(n)
-    error = cp.sum_squares(y-x)/cp.sum(y)
+def best_FPRank(results, params, criterion = 'pmean', toRank=True, debug=False):
+    if toRank:
+        parity = 1 - np.array(results['wrd'])
+    else:
+        parity = 1 - np.array(results['wsd'])
 
-    obj = cp.Minimize( error) 
-    constraints = [0 <= x, W@x  <=x  ]
-    prob = cp.Problem(obj, constraints)
-    prob.solve(solver= solver)#,adaptive_rho_interval = 1)
-    h = x.value
-    h = binarize(h,acceptance_n)
-    return h
+    precs = np.array(results['precision'])
+    fp = np.array(results['fairperception'])
+    corr = np.array(results['corr'])
+              
+    utils = list()
+    for i in range(precs.shape[0]):
+        if criterion == 'pmean':
+            util = power_mean([corr[i], fp[i], parity[i]], 3)
+        elif criterion == 'amean':
+            util = (corr[i] + fp[i] + parity[i])/3
+        else:
+            util = np.min([corr[i], fp[i], parity[i]])
+            
+        utils.append(util)
+        if debug:
+            print('Param = {:.4f},{:.4f} util={:.4f} | prec={:.4f}, corr={:.4f}, parity={:.4f}, fp={:.4f}'.format(params['alpha'][i], params['beta'][i], util, precs[i], corr[i], parity[i], fp[i]))
+        
+    bestparam = np.argmax(utils)
+    print('\nBest parameters: alpha = {:.4f}, beta = {:.4f} (util = {:.4f})'.format(params['alpha'][bestparam], params['beta'][bestparam], utils[bestparam]))
+    return results['h'][bestparam], bestparam, utils 
 
+###########################################
+# Baseline: FSPR Algorithm
+###########################################
+
+def FSPR_model(adj, s, prt, gamma=0.15):
+    N = s.shape[0]
+    phi = prt.sum()/N
+    
+    norm_adj = np.diag(1/(np.sum(adj, axis = 1)+np.finfo(np.float32).eps)).dot(adj)
+    Q = gamma*np.linalg.inv(np.eye(N)-(1-gamma)*norm_adj)
+    qr = np.dot(Q, prt)
+    A = matrix(np.vstack((qr, np.ones(N))))
+    b = matrix(np.array([phi, 1]).reshape(-1,1))
+    
+    QP = matrix(2*Q*Q.T)
+    p = -2*matrix(np.dot(Q,s))
+    G = matrix(np.vstack((-1*np.eye(N), np.eye(N))))
+    h = matrix(np.hstack((np.zeros(N), np.ones(N))))
+    
+    sol=solvers.qp(QP, p, G, h, A, b)
+    return np.array(sol['x']) 
